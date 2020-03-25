3b9a960d335af95720b8ff42e520fc898c187ecd
Sven Steinert <svensteinert96@gmail.com>
2020-03-25 14:45:17 +0100

    prototype

--- game.py
+++ game.py
@@ -1,7 +1,36 @@
 # Datafield
-# Playeraction
 
-import numpy
 
-a = numpy.zeros((6, 7))
-print(a)
+import numpy as np
+
+class game:
+    def __init__(
+            self,
+            board_width=6,
+            board_height=7
+            ):
+        self.board_width = board_width
+        self.board_height = board_height
+        self.board = np.zeros((board_height,board_width))
+        print(self.board)
+        
+        
+    def playeraction(
+            self,
+            player_id,
+            column
+            ):
+        if (column >self.board_width):
+            return print("INVALID")
+        
+        for i in reversed( range(self.board_height)):
+            if self.board[i,column] == 0:
+                self.board[i,column] = player_id
+                return
+
+        print("INVALID")
+        
+
+    def display(self):
+        print(self.board)
+        
--- ui.py
+++ ui.py
@@ -1,3 +1,10 @@
 # imports game.py
 # calls playerfunction
 # displays gamedata
+from game import game
+
+instance1 = game(7,6)
+instance1.playeraction(1,4)
+instance1.display()
+instance1.playeraction(2,4)
+instance1.display()
