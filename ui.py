e64fd351194e388a0e9a7d6eecc9c8b38f753f16
Sven Steinert <svensteinert96@gmail.com>
2020-03-25 16:35:58 +0100

    Game Implementation

--- ui.py
+++ ui.py
@@ -4,7 +4,15 @@
 from game import game
 
 instance1 = game(7,6)
-instance1.playeraction(1,4)
-instance1.display()
-instance1.playeraction(2,4)
+# while True:
+#     instance1.playeraction(1,int(input()))
+#     instance1.display()
+#     instance1.playeraction(2,int(input()))
+#     instance1.display()
+    
+instance1.playeraction(1,6)
+instance1.playeraction(1,6)
+instance1.playeraction(1,6)
+print(" ")
+print(instance1.playeraction(1,6))
 instance1.display()
